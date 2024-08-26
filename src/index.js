9498b5f977cfba90836b6b4d929b87614144dba5
cloudpc <cloudp7@hotmail.com>
2024-08-25 20:53:38 -0700

    The App Component

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import './index.css';
-import 'bootstrap/dis/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-social/bootstrap-social.css';
 import 'font-awesome/css/font-awesome.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
