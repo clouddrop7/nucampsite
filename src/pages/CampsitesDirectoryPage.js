7116784016edc25f799a089ad58db6e726e3e7de
cloudpc <cloudp7@hotmail.com>
2024-08-27 17:10:01 -0700

    Breadcrumbs SubHeader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,14 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsiteDirectoryPage = () => {
 
     return (
         <Container>
+            <SubHeader 
+                current='Home'
+            />
             <CampsitesList />
         </Container>
     )
