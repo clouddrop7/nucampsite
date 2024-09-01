6114a453470806bd64c3bb8fa9c7d5346742ab4e
cloudpc <cloudp7@hotmail.com>
2024-09-01 10:40:47 -0700

    AnimatedDisplayCard

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -2,17 +2,19 @@ import { Row, Col } from 'reactstrap';
 import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import { selectFeaturedPartner } from '../partners/partnersSlice';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = [selectFeaturedCampsite(), selectFeaturedPromotion(),selectFeaturedPartner()];
     return (
         <Row>
             {items.map((item, idx) => (
-                <Col md className='m-1' key={idx}>
-                    <DisplayCard
-                        item={item}
-                    />
-                </Col>
+                item && (
+                    <Col md className='m-1' key={idx}>
+                        <AnimatedDisplayCard item={item }/>
+                    </Col>
+                )
             ))}
         </Row>
     )
